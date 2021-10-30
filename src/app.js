936aa6ca89eeaaa0458a2f57b36127b9a099d7d3
Everton-Victor <evertonvictor2019br@gmail.com>
2021-10-30 18:43:09 -0300

    Gravação de dados
    Foi criado a classe Bd que permite cadastrar diversas despesas no localStorage sem sobreescrever

--- src/app.js
+++ src/app.js
@@ -7,7 +7,44 @@ class Despesa {
       (this.descricao = descricao),
       (this.valor = valor)
   }
+
+  validarDados() {
+    for (let indice in this) {
+      // this se trata do proprio objeto despesa
+      if (
+        this[indice] === undefined ||
+        this[indice] === '' ||
+        this[indice] === null
+      ) {
+        return false
+      }
+    }
+
+    return true
+  }
 }
+
+class Bd {
+  constructor() {
+    let id = localStorage.getItem('id')
+
+    if (id === null) {
+      localStorage.setItem('id', 0)
+    }
+  }
+  getProximoId() {
+    let proximoId = parseInt(localStorage.getItem('id')) + 1
+    return proximoId
+  }
+
+  gravar(despesa) {
+    let id = this.getProximoId()
+    localStorage.setItem(id, JSON.stringify(despesa))
+    localStorage.setItem('id', id)
+  }
+}
+
+let bd = new Bd()
 
 function cadastrarDespesa() {
   let ano = document.getElementById('ano')
@@ -26,5 +63,19 @@ function cadastrarDespesa() {
     valor.value
   )
 
-  console.log(despesa)
+  if (despesa.validarDados()) {
+    bd.gravar(despesa)
+    $('#sucessoGravacao').modal('show')
+
+    {
+      document.getElementById('ano').value = ''
+      document.getElementById('mes').value = ''
+      document.getElementById('dia').value = ''
+      document.getElementById('tipo').value = ''
+      document.getElementById('descricao').value = ''
+      document.getElementById('valor').value = ''
+    }
+  } else {
+    $('#erroGravacao').modal('show')
+  }
 }
